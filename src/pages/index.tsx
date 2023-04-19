59a8e5c40f512e2fe77a766f5b379140c90e595b
pogadev <pogadev18@gmail.com>
2023-04-19 16:15:29 +0300

    reusable modal component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+import { MdProductionQuantityLimits } from "react-icons/md";
 
 import { META_DESCRIPTION, META_TITLE } from "~/utils/constants";
 import PageLayout from "~/components/layout";
+import Modal from "~/components/modal";
+import { strings } from "~/utils/strings";
 
 const Home: NextPage = () => {
+  const [showModal, setShowModal] = useState(false);
+  const { ADD_PRODUCT_CAT_MODAL_TITLE, ADD_PRODUCT_CAT_MODAL_DESCRIPTION } =
+    strings;
+
   return (
     <>
       <Head>
@@ -13,7 +21,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageLayout>
-        <p>hello</p>
+        <section className="mt-5">
+          <button
+            onClick={() => setShowModal(true)}
+            className="rounded bg-lime-600 p-2 text-white transition-all hover:bg-lime-700 "
+          >
+            add product category
+          </button>
+          <Modal
+            icon={
+              <MdProductionQuantityLimits
+                className="h-6 w-6 text-white"
+                aria-hidden="true"
+              />
+            }
+            title={ADD_PRODUCT_CAT_MODAL_TITLE}
+            description={ADD_PRODUCT_CAT_MODAL_DESCRIPTION}
+            open={showModal}
+            setOpen={setShowModal}
+          >
+            <p>add categories</p>
+          </Modal>
+        </section>
       </PageLayout>
     </>
   );
---
+++ src/utils/strings.ts
@@ -0,0 +1,8 @@
+export const strings = {
+  ADD_PRODUCT_MODAL_TITLE: "Add Product",
+  ADD_PRODUCT_MODAL_DESCRIPTION:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente deserunt quae voluptas ex aperiam consequatur unde aut, eaque ut excepturi.",
+  ADD_PRODUCT_CAT_MODAL_TITLE: "Add a product category",
+  ADD_PRODUCT_CAT_MODAL_DESCRIPTION:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente deserunt quae voluptas ex aperiam consequatur unde aut, eaque ut excepturi.",
+};
